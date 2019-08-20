286b92beb92279ad84507dc8e77270ec642f070c
Ryan Berkun <45776981+rbcp18@users.noreply.github.com>
2019-08-19 18:55:20 -0700

    updating index.py file

--- livepeer-api-open-source/src-python/index.py
+++ livepeer-api-open-source/src-python/index.py
@@ -3,6 +3,7 @@ from flask_restful import Api, Resource, reqparse
 from flask_sslify import SSLify
 from resources.livepeer_subgraph import livepeer_subgraph_get_calls
 from resources.livepeer_staking_alerts import livepeer_staking_alerts
+from resources.livepeer_general_info import livepeer_general_info_get_calls
 
 app = Flask(__name__)
 app.config['RESTFUL_JSON'] = {'indent':None, 'separators':(',',':')}
@@ -11,6 +12,7 @@ api = Api(app)
 
 api.add_resource(livepeer_subgraph_get_calls, "/livepeer/<string:service>")
 api.add_resource(livepeer_staking_alerts, "/livepeer/staking-alerts/confirmEmail")
+api.add_resource(livepeer_general_info_get_calls, "/livepeer/info/<string:service>")
 
 #Development
 app.run(debug=True)
