958445638a20dd15dc95295ef875f46ce68cba90
Ryan Berkun <45776981+rbcp18@users.noreply.github.com>
2019-08-23 15:55:43 -0700

    Add files via upload

--- livepeer-api-open-source/src-python/index.py
+++ livepeer-api-open-source/src-python/index.py
@@ -5,6 +5,7 @@ from resources.livepeer_subgraph import livepeer_subgraph_get_calls
 from resources.livepeer_staking_alerts import livepeer_staking_alerts
 from resources.livepeer_general_info import livepeer_general_info_get_calls
 from resources.livepeer_lpt_transactions import livepeer_lpt_transactions
+from resources.livepeer_exchange_data import livepeer_exchange_data
 
 app = Flask(__name__)
 app.config['RESTFUL_JSON'] = {'indent':None, 'separators':(',',':')}
@@ -15,6 +16,7 @@ api.add_resource(livepeer_subgraph_get_calls, "/livepeer/<string:service>")
 api.add_resource(livepeer_staking_alerts, "/livepeer/staking-alerts/confirmEmail")
 api.add_resource(livepeer_general_info_get_calls, "/livepeer/info/<string:service>")
 api.add_resource(livepeer_lpt_transactions, "/livepeer/transactions/<string:service>")
+api.add_resource(livepeer_exchange_data, "/livepeer/exchanges/<string:service>")
 
 #Development
 app.run(debug=True)
